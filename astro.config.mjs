92043cd38e5b978bb0d74f3941d320e8f68bcd1c
Johan <hal215@outlook.com>
2026-01-22 05:24:00 -0600

    Added Tailwind, organized UI a bit more and added more styling. Added a few more stretches

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import tailwindcss from '@tailwindcss/vite';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://boole215.github.io',
   base: '/stretch-rx',
+
   image: {
     service: {
       entrypoint: 'astro/assets/services/sharp',
@@ -12,5 +15,9 @@ export default defineConfig({
         limitInputPixels: false,
       },
     },
-  },  
+  },
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
